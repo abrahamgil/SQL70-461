b7c31564e8270f642e42f00784c5b919b5abca50
abrahamgil <abrahamgil2@gmail.com>
2015-09-16 20:52:48 -0500

    more insert examples

--- INSERT.sql
+++ INSERT.sql
@@ -4,3 +4,11 @@ INSERT INTO Production.UnitMeasure
 (UnitMeasureCode,Name,ModifiedDate)
 VALUES
 ('FT', 'Feet', '20080414')
+
+INSERT INTO dbo.EmployeeSales
+SELECT 'SELECT', sp.BusinessEntityID, c.LastName, sp.SalesYTD
+FROM Sales.SalesPerson AS sp
+INNER JOIN Person.Person AS c
+ON sp.BusinessEntityID = c.BusinessEntityID
+WHERE sp.BusinessEntityID LIKE '2%'
+ORDER BY sp.BusinessEntityID, c.LastName;
